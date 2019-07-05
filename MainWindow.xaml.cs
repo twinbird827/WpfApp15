3e3d03de91d8e2f85da8e463dbc1ce3f77d18699
twinbird827 <twinbird827@gmail.com>
2019-07-05 11:16:42 +0900

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp15
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Text1 = string.Join("", Enumerable.Range(0, 100).Select(i => "W"));
+            Text2 = string.Join("", Enumerable.Range(0, 200).Select(i => "W"));
+            Text3 = string.Join("", Enumerable.Range(0, 300).Select(i => "W"));
+
+            DataContext = this;
+        }
+
+        public string Text1 { get; set; }
+        public string Text2 { get; set; }
+        public string Text3 { get; set; }
+    }
+}
---
+++ TextBlockWrappingBehavior.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApp15
+{
+    public static class TextBlockWrappingBehavior
+    {
+        /// <summary>
+        /// ﾃｷｽﾄﾌﾞﾛｯｸに表示可能な最大行数の依存関係ﾌﾟﾛﾊﾟﾃｨ
+        /// </summary>
+        public static DependencyProperty MaxLinesProperty =
+            DependencyProperty.RegisterAttached("MaxLines",
+                typeof(int),
+                typeof(TextBlockWrappingBehavior),
+                new FrameworkPropertyMetadata(int.MinValue, OnSetMaxLinesCallback)
+            );
+
+        /// <summary>
+        /// ﾃｷｽﾄﾌﾞﾛｯｸに表示可能な最大行数を設定します（添付ﾋﾞﾍｲﾋﾞｱ）
+        /// </summary>
+        /// <param name="target">対象</param>
+        /// <param name="value">ﾃｷｽﾄﾌﾞﾛｯｸに表示可能な最大行数</param>
+        public static void SetMaxLines(DependencyObject target, object value)
+        {
+            target.SetValue(MaxLinesProperty, value);
+        }
+
+        /// <summary>
+        /// ﾃｷｽﾄﾌﾞﾛｯｸに表示可能な最大行数を取得します（添付ﾋﾞﾍｲﾋﾞｱ）
+        /// </summary>
+        /// <param name="target">対象</param>
+        public static int GetMaxLines(DependencyObject target)
+        {
+            return (int)target.GetValue(MaxLinesProperty);
+        }
+
+        /// <summary>
+        /// MaxLinesﾌﾟﾛﾊﾟﾃｨが変更された際の処理
+        /// </summary>
+        private static void OnSetMaxLinesCallback(DependencyObject target, DependencyPropertyChangedEventArgs e)
+        {
+            var block = target as TextBlock;
+
+            if (block == null)
+            {
+                return;
+            }
+            if (block.TextWrapping == TextWrapping.NoWrap)
+            {
+                // 改行設定
+                block.TextWrapping = TextWrapping.Wrap;
+            }
+            if (block.TextTrimming == TextTrimming.None)
+            {
+                // 省略文字設定
+                block.TextTrimming = TextTrimming.CharacterEllipsis;
+            }
+
+            var binding = BindingOperations.GetBinding(block, TextBlock.TextProperty);
+
+            if (binding == null)
+            {
+                return;
+            }
+            if (!binding.NotifyOnTargetUpdated)
+            {
+                // Textﾌﾟﾛﾊﾟﾃｨ変更時に通知されるようにする
+                BindingOperations.SetBinding(block, TextBlock.TextProperty, new Binding()
+                {
+                    Path = binding.Path,
+                    NotifyOnTargetUpdated = true
+                });
+            }
+
+            block.TargetUpdated += (sender, args) =>
+            {
+                // 行の高さが設定されている場合はその値、未設定であればFormattedTextの高さを行の高さとする
+                var line = double.IsInfinity(block.LineHeight) || double.IsNaN(block.LineHeight)
+                    ? GetFormattedTextHeight(block)
+                    : block.LineHeight;
+
+                // 最大高さ設定
+                block.MaxHeight = line * GetMaxLines(block);
+            };
+        }
+
+        /// <summary>
+        /// TextBlockの文字をFormattedText化して、その高さを取得します。
+        /// </summary>
+        /// <param name="block">ｵﾌﾞｼﾞｪｸﾄ</param>
+        private static double GetFormattedTextHeight(TextBlock block)
+        {
+            var formatted = new FormattedText(block.Text ?? string.Empty,
+                new CultureInfo("ja-jp"),
+                FlowDirection.LeftToRight,
+                new Typeface(block.FontFamily, block.FontStyle, block.FontWeight, block.FontStretch),
+                block.FontSize,
+                block.Foreground
+            );
+            return formatted.Height;
+        }
+    }
+}
